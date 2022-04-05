4acca93a054d3e7ce5dbf8159aaa1d6029cf5658
Hugo-NF <hugo.fonseca@grupoorion.com.br>
2022-04-05 20:56:05 -0300

    🚧 Import hero cover from assets

--- src/components/ProductHero/index.tsx
+++ src/components/ProductHero/index.tsx
@@ -2,20 +2,20 @@ import Button from '../Button';
 import Typography from '../Typography';
 import ProductHeroLayout from './layout';
 
-const backgroundImage = 'https://images.unsplash.com/photo-1620059409361-37e0047f829e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1471&q=80';
+import { Images } from '../../constants';
 
 const ProductHero = () => (
   <ProductHeroLayout
     sxBackground={{
-      backgroundImage: `url(${backgroundImage})`,
-      backgroundColor: '#7fc7d9', // Average color of the background image.
+      backgroundImage: `url(${Images.HeroCover})`,
+      backgroundColor: '#d3dadb', // Average color of the background image.
       backgroundPosition: 'center',
     }}
   >
     {/* Increase the network loading priority of the background image. */}
     <img
       style={{ display: 'none' }}
-      src={backgroundImage}
+      src={Images.HeroCover}
       alt="increase priority"
     />
     <Typography color="inherit" align="center" variant="h2" marked="center">
