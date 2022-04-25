5292c301a354ff36760a0d2953e4fe850183be71
Hugo-NF <hugo.fonseca@grupoorion.com.br>
2022-04-25 14:34:01 -0300

    Basic grid to Contact Form

--- src/components/ContactUs/index.tsx
+++ src/components/ContactUs/index.tsx
@@ -1,8 +1,9 @@
 import { forwardRef, useCallback } from 'react';
 import {
   Box,
+  Button,
+  Grid,
   TextField,
-  // Grid,
   Typography,
   useTheme,
 } from '@mui/material';
@@ -48,17 +49,83 @@ const ContactUs = forwardRef((_, ref) => {
         <Typography variant="body1">Deseja fazer o orçamento de algum Medicamento de Alto Custo?</Typography>
         <Typography variant="body1">Este é o lugar certo.</Typography>
         <form onSubmit={formik.handleSubmit}>
-          <TextField
-            id="email"
-            label="E-mail"
-            type="email"
-            variant="standard"
-            color="secondary"
-            onChange={formik.handleChange}
-            value={formik.values.email}
-            sx={{ backgroundColor: 'transparent' }}
-          />
-          <button type="submit">Submit</button>
+          <Grid
+            container
+            direction="column"
+            justifyContent="center"
+            alignItems="center"
+            spacing={4}
+          >
+            {/* Name and phone row */}
+            <Grid container columnSpacing={20} my={5}>
+              <Grid item xs={6}>
+                <TextField
+                  id="name"
+                  label="Nome"
+                  type="name"
+                  variant="standard"
+                  fullWidth
+                  onChange={formik.handleChange}
+                  value={formik.values.name}
+                />
+              </Grid>
+              <Grid item xs={6}>
+                <TextField
+                  id="phone"
+                  label="Telefone"
+                  type="tel"
+                  variant="standard"
+                  color="secondary"
+                  onChange={formik.handleChange}
+                  value={formik.values.phone}
+                />
+              </Grid>
+            </Grid>
+            {/* E-mail row */}
+            <Grid container>
+              <Grid item xs>
+                <TextField
+                  id="email"
+                  label="E-mail"
+                  type="email"
+                  variant="standard"
+                  color="secondary"
+                  fullWidth
+                  onChange={formik.handleChange}
+                  value={formik.values.email}
+                />
+              </Grid>
+            </Grid>
+            {/* Message row */}
+            <Grid container my={5}>
+              <Grid item xs>
+                <TextField
+                  id="message"
+                  label="Mensagem"
+                  type="message"
+                  fullWidth
+                  multiline
+                  rows={5}
+                  variant="standard"
+                  color="secondary"
+                  onChange={formik.handleChange}
+                  value={formik.values.message}
+                />
+              </Grid>
+            </Grid>
+            {/* Submit Button row */}
+            <Grid container>
+              <Grid item xs>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="secondary"
+                >
+                  Enviar
+                </Button>
+              </Grid>
+            </Grid>
+          </Grid>
         </form>
       </ContactUsContainer>
     </Box>
